1735337981173bcc4e4522ffd0d31227667ed374
Everton Espedito <evertondev505@gmail.com>
2025-05-29 22:27:20 -0300

    update
    add info.py e add styles.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sys
 
 from main_window import MainWindow
 from display import Display
+from info import Info
+from styles import setupTheme
 from PySide6.QtWidgets import QApplication, QLineEdit
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
@@ -10,12 +12,18 @@ from variables import WINDOW_ICON_PATH
 if __name__ == '__main__':
     # Cria a aplicação
     app = QApplication(sys.argv)
+    setupTheme()
     window = MainWindow()
 
     # Define o ícone
     icon = QIcon(str(WINDOW_ICON_PATH))
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
+
+    #Info
+    info = Info("2.0 ^ 10.0 = 1024.0")
+    window.addToVLayout(info)
+    
 
     # Display
     display = Display()
